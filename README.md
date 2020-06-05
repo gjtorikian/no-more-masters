0616cd154a6fd4e3e83b0c784107d71782adf519
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2020-06-05 12:51:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+no-more-masters
+=============
+
+Rename your default git branches from production
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/no-more-masters.svg)](https://npmjs.org/package/no-more-masters)
+[![Downloads/week](https://img.shields.io/npm/dw/no-more-masters.svg)](https://npmjs.org/package/no-more-masters)
+[![License](https://img.shields.io/npm/l/no-more-masters.svg)](https://github.com/gjtorikian/no-more-masters/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g no-more-masters
+$ nmm COMMAND
+running command...
+$ nmm (-v|--version|version)
+no-more-masters/0.0.0 darwin-x64 node-v12.13.0
+$ nmm --help [COMMAND]
+USAGE
+  $ nmm COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.js
@@ -0,0 +1,97 @@
+const { Command, flags } = require("@oclif/command");
+const { cli } = require("cli-ux");
+const execa = require("execa");
+const axios = require("axios");
+const parseRepo = require("parse-repo");
+const util = require("util");
+
+class nomoremastersCommand extends Command {
+  async run() {
+    const { flags } = this.parse(nomoremastersCommand);
+
+    let origin = "";
+    try {
+      const { stdout } = await execa("git", ["remote", "get-url", "origin"]);
+      origin = stdout;
+    } catch (error) {
+      this.error(error);
+      return;
+    }
+
+    const nwo = parseRepo(origin).repository;
+
+    this
+      .log(`This script will create a new branch off of master called production.
+
+It will then set production as the default branch on GitHub, and then delete the master branch.
+
+This will modify the ${nwo} repository.
+
+These are DESTRUCTIVE actions!
+  `);
+    const choice = await cli.prompt("Do you understand [y/n]?");
+    if (choice != "y") {
+      this.log(
+        `I expected you to say 'y' but you did not, so I won't do anything.`
+      );
+      return;
+    }
+
+    const token = await cli.prompt("What is your GitHub API token?", {
+      type: "hide",
+    });
+
+    try {
+      const { stdout } = await execa("git", [
+        "checkout",
+        "-b",
+        "production",
+        "master",
+      ]);
+      console.log(stdout);
+    } catch (error) {
+      this.error(error);
+      return;
+    }
+
+    try {
+      const { stdout } = await execa("git", ["push", "origin", "production"]);
+      console.log(stdout);
+    } catch (error) {
+      this.error(error);
+      return;
+    }
+
+    this.log(`Modifying https://api.github.com/repos/${nwo} ...`);
+    try {
+      const data = { default_branch: "production" };
+      const options = {
+        method: "PATCH",
+        headers: { Authorization: `token ${token}` },
+        data: data,
+        url: `https://api.github.com/repos/${nwo}`,
+      };
+
+      await axios(options);
+    } catch (e) {
+      this.error(
+        util.inspect(e.response.data, { showHidden: false, depth: null })
+      );
+      return;
+    }
+
+    this.log(`All done! PS: GitHub, drop ICE 🤗`);
+  }
+}
+
+nomoremastersCommand.description =
+  "Use this script to rename your default Git branches from `master` to `production`";
+
+nomoremastersCommand.flags = {
+  // add --version flag to show CLI version
+  version: flags.version({ char: "v" }),
+  // add --help flag to show CLI version
+  help: flags.help({ char: "h" }),
+};
+
+module.exports = nomoremastersCommand;
